cb61479fcd5516bff0b175a44fcf23b804eceabb
martinchakarov <61684682+martinchakarov@users.noreply.github.com>
2020-12-13 11:49:58 +0200

    Added header to home page

--- scripts/components/home.js
+++ scripts/components/home.js
@@ -1,9 +1,24 @@
+import { html, render } from 'https://unpkg.com/lit-html?module';
+
+const template = () => html`
+            <div class="jumbotron jumbotron-fluid text-light" style="background-color: #343a40;">
+                <img src="https://s.studiobinder.com/wp-content/uploads/2019/06/Best-M-Night-Shyamalan-Movies-and-Directing-Style-StudioBinder.jpg"
+                    class="img-fluid" alt="Responsive image">
+                <h1 class="display-4">Movies</h1>
+                <p class="lead">Unlimited movies, TV shows, and more. Watch anywhere. Cancel anytime.</p>
+            </div>
+`
+
 export default class Home extends HTMLElement {
     constructor() {
         super();
     }
 
     connectedCallback() {
-        console.log('in home component');
+        this.render();
+    }
+
+    render() {
+        render(template(), this);
     }
 }
