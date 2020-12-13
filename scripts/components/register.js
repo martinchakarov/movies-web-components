64d09f71007c4bc2c8f98cdd195ec5eaec5ca15a
martinchakarov <61684682+martinchakarov@users.noreply.github.com>
2020-12-13 11:43:23 +0200

    Added basic simple components to render

---
+++ scripts/components/register.js
@@ -0,0 +1,9 @@
+export default class Register extends HTMLElement{
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        console.log('in register component');
+    }
+}
