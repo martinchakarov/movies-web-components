628f915f5e3df23a0883ab979db6a93887494d06
martinchakarov <61684682+martinchakarov@users.noreply.github.com>
2020-12-13 11:53:00 +0200

    Added register page

--- scripts/components/register.js
+++ scripts/components/register.js
@@ -1,9 +1,35 @@
+import { html, render } from 'https://unpkg.com/lit-html?module';
+
+const template = () => html`
+        <form class="text-center border border-light p-5" action="#" method="post">
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" placeholder="Email" name="email" value="">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" placeholder="Password" name="password" value="">
+            </div>
+        
+            <div class="form-group">
+                <label for="repeatPassword">Repeat Password</label>
+                <input type="password" class="form-control" placeholder="Repeat-Password" name="repeatPassword" value="">
+            </div>
+        
+            <button type="submit" class="btn btn-primary">Register</button>
+        </form>
+`
+
 export default class Register extends HTMLElement{
     constructor() {
         super();
     }
 
     connectedCallback() {
-        console.log('in register component');
+        this.render();
+    }
+
+    render() {
+        render(template(), this);
     }
 }
